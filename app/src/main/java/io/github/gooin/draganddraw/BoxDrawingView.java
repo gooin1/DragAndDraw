959decfb6b2bff09f9931890d280517030883ac2
gooin <gooin@outlook.com>
2016-07-11 22:25:07 +0800

    空视图

---
+++ app/src/main/java/io/github/gooin/draganddraw/BoxDrawingView.java
@@ -0,0 +1,19 @@
+package io.github.gooin.draganddraw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by gooin on 2016/7/11.
+ */
+public class BoxDrawingView extends View {
+
+    public BoxDrawingView(Context context) {
+        this(context, null);
+    }
+
+    public BoxDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
