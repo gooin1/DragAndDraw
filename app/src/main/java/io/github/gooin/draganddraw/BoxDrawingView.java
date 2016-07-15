f360ad5b70d64c01d66ce8534919a436af801308
gooin <gooin@outlook.com>
2016-07-15 21:39:20 +0800

    log窗口中可以查看当前触摸坐标

--- app/src/main/java/io/github/gooin/draganddraw/BoxDrawingView.java
+++ app/src/main/java/io/github/gooin/draganddraw/BoxDrawingView.java
@@ -1,13 +1,19 @@
 package io.github.gooin.draganddraw;
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
  * Created by gooin on 2016/7/11.
  */
 public class BoxDrawingView extends View {
+
+    private static final String TAG = "BoxDrawingView";
+
 
     public BoxDrawingView(Context context) {
         this(context, null);
@@ -16,4 +22,33 @@ public class BoxDrawingView extends View {
     public BoxDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(), event.getY());
+        String action = "";
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                break;
+        }
+        Log.i(TAG, action + " at x=" + current.x +
+                ", y=" + current.y);
+        return true;
+    }
+
+
+
+
 }
+
